787175d5e0aab407c7cb6940e63c7e37d22c3385
Dragan <s.dragan.95@outlook.it>
2022-10-04 20:55:25 +0200

    add meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = props => {
+
+    return (
+        <main>  
+            <MealsSummary />
+            <AvailableMeals />
+        </main>
+    )
+}
+export default Meals;
